5816c8a3d946d55ac863d9936ce1edb0efe6d5a1
Vittorio Carmignani <carmignani.vittorio@gmail.com>
2019-03-28 16:12:51 +0100

    comment

--- __init__.py
+++ __init__.py
@@ -7,18 +7,18 @@ from gym.envs.registration import register
 
 # Pretty sure this is the wrong way to go about this problem. How can we apply custom wrappers in the openai baseline
 # framework?
-for i in range(8):
-    for frame_size in [64, 96]:
-        register(
-            id='doom_scenario%i_%i-v0'%(i, frame_size),
-            entry_point='gdoom:WGDoomEnv',
-            kwargs = {'level': i, 'frame_size': frame_size, 'mode': CPU}
-        )
-    # human play
-    register(
-        id='doom_scenario%i_human-v0' % (i,),
-        entry_point='gdoom:WGDoomEnv',
-        kwargs={'level': i, 'mode': HUMAN}
-    )
-
-    a = 234
+# for i in range(8):
+#     for frame_size in [64, 96]:
+#         register(
+#             id='doom_scenario%i_%i-v0'%(i, frame_size),
+#             entry_point='gdoom:WGDoomEnv',
+#             kwargs = {'level': i, 'frame_size': frame_size, 'mode': CPU}
+#         )
+#     # human play
+#     register(
+#         id='doom_scenario%i_human-v0' % (i,),
+#         entry_point='gdoom:WGDoomEnv',
+#         kwargs={'level': i, 'mode': HUMAN}
+#     )
+#
+#     a = 234
